37b1283681f800e982c3b778b1140a96a8ec1100
Alexander Komarov <ak@akxcv.com>
2017-05-14 13:17:02 +0300

    [ESLint] Separate configs for standard, Flow and React

--- eslint/index.js
+++ eslint/index.js
@@ -1,19 +1,5 @@
 module.exports = {
-  plugins: [
-    'curry',
-    'flowtype',
-    'import',
-    'jsx-a11y',
-    'node',
-    'prefer-object-spread',
-    'promise',
-    'react',
-  ],
-  extends: [
-    './rules/es6',
-    './rules/etc',
-    './rules/import',
-    './rules/react',
-    './rules/flowtype',
-  ].map(require.resolve),
+  plugins: ['curry', 'import', 'node', 'prefer-object-spread', 'promise'],
+  extends: ['./rules/es6', './rules/etc', './rules/import'].map(require.resolve),
 }
+
